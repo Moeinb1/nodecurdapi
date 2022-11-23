fb628c85eeb3fb9b9ad591cf2d127174685efd92
Moein Bagheri <moeinbagheri4@gmail.com>
2022-11-23 18:30:56 +0330

    test get todos rows

--- server/server.js
+++ server/server.js
@@ -23,10 +23,20 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e)
     })
 
+})
 
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e)
+    })
 })
 
 app.listen(3000, () => {
     console.log('start on port 3000')
 })
+
+
+
+module.exports = { app }
