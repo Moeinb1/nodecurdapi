3860267c9870e44666b7f79d4a6573848f82aac4
Moein Bagheri <moeinbagheri4@gmail.com>
2022-11-24 18:54:19 +0330

    GET BY ID

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var { User } = require('./models/user')
 
 var express = require('express')
 var bodyparser = require('body-parser')
-
+var { ObjectID } = require('mongodb')
 var app = express();
 
 app.use(bodyparser.json());
@@ -21,6 +21,26 @@ app.post('/todos', (req, res) => {
         res.status(200).send(doc)
     }, (e) => {
         res.status(400).send(e)
+    })
+
+})
+
+
+/// http://yoursite.com/ldsmlmas
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({ todo })
+    }).catch((e) => {
+        res.status(400).send();
     })
 
 })
